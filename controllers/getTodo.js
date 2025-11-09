cec93494d7f744451e32a9e23683419241182548
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-11-10 01:30:08 +0530

    getrequest

---
+++ controllers/getTodo.js
@@ -0,0 +1,64 @@
+  //import the model
+  const Todo =require("../models/Todo");
+  
+  //define route handler
+  
+  exports.getTodo = async (req,res) => {
+      try{
+          //fetch all todo entries from database
+          const todo =await Todo.find({});
+
+          //response with json data
+          res.status(200)
+          .json({
+            success:true,
+            data:todo,
+            message:"Entire rodo data is featched successfully",
+      })
+    }
+      catch(err0r){
+          console.log(err);
+          res.ststus(500)
+          .json({
+            success:false,
+            error:err.message,
+            message:"server error"
+           })
+  
+      }
+  }
+
+  exports.getTodobyId = async (req,res) =>{
+    try{
+        //extract todo item on the basis of id
+        const id= req.params.id;
+        const todo = await Todo.findById({_id:id});
+
+        //data forgiven id not found
+        if(!todo){
+            return res.status(404).json({
+                success:false,
+                message:"no data found woth given id"
+            })
+        }
+
+        //data for given id found
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message:"Todo ${id} data successfully faetched",
+        })
+    
+ 
+    }
+    catch(err) {
+          console.log(err);
+          res.ststus(500)
+          .json({
+            success:false,
+            error:err.message,
+            message:"server error"
+           })
+  
+      }
+  }
--- routes/todos.js
+++ routes/todos.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 //import the controller
 const {createTodo}= require("../controllers/createTodo");
-const {getTodo}= require("../controllers/getTodo");
+const {getTodo,getTodobyId}= require("../controllers/getTodo");
 
 //define API routes
 router.post("/createTodo", createTodo); 
-router.get("/getTodos",getTodo); 
+router.get("/getTodos",getTodo);  
+router.get("/getTodos/:id",getTodobyId)
 module.exports = router;
